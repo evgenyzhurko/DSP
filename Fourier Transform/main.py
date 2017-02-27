9a129da102ce0407ead77096abaa081462686f12
Evgeny Zhurko <evgeny.zhurko@gmail.com>
2017-02-27 19:02:27 +0300

    ENHANCEMENT: continuous function for y and z

--- Fourier Transform/main.py
+++ Fourier Transform/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf as pdf
 
 N = 16
+n_cont = 100
 
 
 def y(x):
@@ -18,6 +19,9 @@ def z(x):
 y_val = [y(2 * np.pi * i / N) for i in xrange(N)]
 z_val = [z(2 * np.pi * i / N) for i in xrange(N)]
 
+y_cont = [y(2 * np.pi * i / n_cont) for i in xrange(n_cont)]
+z_cont = [z(2 * np.pi * i / n_cont) for i in xrange(n_cont)]
+
 
 correlation = corr(y_val, z_val, N)
 
@@ -29,22 +33,22 @@ convolution_fft = conv_fft(y_val, z_val, N)
 
 with pdf.PdfPages('results.pdf') as file:
 
-    plt.plot(np.arange(N), y_val)
+    plt.plot(np.arange(n_cont), y_cont)
     plt.title("Y function")
     plt.xlabel('x')
     plt.ylabel('y=sin(2x)')
     file.savefig()
     plt.close()
 
-    plt.plot(np.arange(N), z_val)
+    plt.plot(np.arange(n_cont), z_cont)
     plt.title("Z function")
     plt.xlabel('x')
     plt.ylabel('z=cos(7x)')
     file.savefig()
     plt.close()
 
-    plt.plot(np.arange(N), y_val)
-    plt.plot(np.arange(N), z_val)
+    plt.plot(np.arange(n_cont), y_cont)
+    plt.plot(np.arange(n_cont), z_cont)
     plt.title("Y + Z function")
     plt.xlabel('x')
     plt.ylabel('y')
